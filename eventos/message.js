47af4bc064697888b5898c9421d39854acab1985
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-23 06:55:57 +0100

    Message.js Actualizado

--- eventos/message.js
+++ eventos/message.js
@@ -1,13 +1,13 @@
-const prefix = "-"  
+const prefijo = "-"  
 
  exports.run = async(client, message) => {
   if (message.author.bot) return;
-  if (message.content.startsWith(prefix)) {
+  if (message.content.startsWith(prefijo)) {
     
  let messageArray = message.content.split(" "),
      cmd = messageArray[0],
      args = messageArray.slice(1),
-     commandfile = client.commands.get(cmd.slice(prefix.length)) || client.aliases.get(cmd.slice(prefix.length));
+     commandfile = client.commands.get(cmd.slice(prefijo.length)) || client.aliases.get(cmd.slice(prefijo.length));
   
 if(!commandfile) return;    
     commandfile.run(client,message,args);             
