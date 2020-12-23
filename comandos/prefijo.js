52b8715dd9f2534f4a98450154b4f2359518cf69
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-23 07:40:48 +0100

    Prefijo.js Actualizado

--- comandos/prefijo.js
+++ comandos/prefijo.js
@@ -1,5 +1,5 @@
-const prefijo = "-"  
+const config = require('../config.json');
 
 exports.run = (client, message, args) => {
-  message.channel.send(`**__Prefijo de el bot__**: **\`${prefijo}\`** :)`);
+  message.channel.send(`**__Prefijo de el bot__**: **\`${config.prefijo}\`** **:)**`);
 }
