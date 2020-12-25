be804d9d520f77c3315917db13046f8e38a0b43f
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-25 16:27:21 +0100

    Ayuda.js Actualizado

--- comandos/ayuda.js
+++ comandos/ayuda.js
@@ -35,6 +35,7 @@ exports.run = (client, message, args) => {
 -------------------------------------------------------
 ⦿ \`${p}\`**__say__**     	    = **\`Comando para enviar un mensaje desde el Bot\`**
 ⦿ \`${p}\`**__sayembed__**          = **\`Comando para enviar un mensaje desde el Bot en un Embed\`**
+⦿ \`${p}\`**__sayuser__**           = **\`Comando para enviar un mensaje con una WebHook desde tu usuario o otro Usuario\`**
 ⦿ \`${p}\`**__eval__**    	    = **\`Comando para realizar una Operacion en Js desde Discord\`**`) 
 	.addField("El Owner Tiene", `**\`3\`** **__Comandos__**`)
 	.setTimestamp()
