f7a1bc56abcce1090e309ab4d87dd09fb5e037d1
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-25 12:41:30 +0100

    Ready.js Actualizado

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,5 +1,6 @@
 const config = require('../config.json');
 
 exports.run = async(client) => {
+  console.log(`Loggeado en ${client.user.tag}`)
   client.user.setActivity(`Usa ${config.prefijo}ayuda`, { type: "WATCHING" });
 }
