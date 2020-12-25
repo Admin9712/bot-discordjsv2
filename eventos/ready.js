54d4bd2aee5d1e5a69b6249537580d7105453f5e
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-25 16:17:59 +0100

    Ready.js Actualizado

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,6 +1,17 @@
+const Discord = require("discord.js");
 const config = require('../config.json');
 
 exports.run = async(client) => {
   console.log(`Loggeado en ${client.user.tag}`)
-  client.user.setActivity(`Usa ${config.prefijo}ayuda`, { type: "WATCHING" });
+  setInterval(() => {
+      let MSGS = [
+          `${config.prefijo}ayuda`,
+          `eAE`
+      ];
+      
+      client.user.setActivity(MSGS[~~(Math.random() * MSGS.length)], {
+          type: 'WATCHING'
+      })
+
+  }, 10000); // TIEMPO EN MILISEGUNDOS
 }
