ca758873248d96da1ee17e33fe00bdcc8cefe7d0
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-27 02:51:29 +0100

    GuildBanRemove Actualizado

--- eventos/guildBanRemove.js
+++ eventos/guildBanRemove.js
@@ -6,10 +6,10 @@ if(!guild.member(client.user).hasPermission('VIEW_AUDIT_LOG')) return;
  guild.fetchAuditLogs().then(logs => { 
   let userID = logs.entries.first().executor.id;
  let embeusds = new Discord.MessageEmbed() 
-    .setTitle('**[USUARIO DESBANEADO]**')
+    .setTitle(`**[${user.bot ? 'BOT' : 'USUARIO'} DESBANEADO]**`)
     .setColor(config.colorEmbeds)
-    .addField("Usuario Desbaneado:", `<@${user.id}>`, true)
-    .addField("ID Usuario Desbaneado:", `**\`${user.id}\`**`, true)
+    .addField(`${user.bot ? 'Bot' : 'Usuario'} Desbaneado:`, `<@${user.id}>`, true)
+    .addField(`ID ${user.bot ? 'Bot' : 'Usuario'} Desbaneado:`, `**\`${user.id}\`**`, true)
     .addField("Staff:", `<@${userID}>`, false)
     .addField("ID Staff:", `**\`${userID}\`**`, true)
     .setTimestamp()
