6a899b7a76384c4c355a0bc5ac29bf466b7b733c
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-27 03:00:28 +0100

    Ready.js Actualizado

--- eventos/ready.js
+++ eventos/ready.js
@@ -3,10 +3,19 @@ const config = require('../config.json');
 
 exports.run = async(client) => {
   console.log(`Loggeado en ${client.user.tag}`)
+ let embedread = new Discord.MessageEmbed() 
+    .setTitle(`**[BOT INICIADO]**`)
+    .setThumbnail("https://images-ext-1.discordapp.net/external/UEu3Cx_CnK2HB4MXx6l1hypTiwG1YWAVO1dIDyDe4h8/https/i.pinimg.com/originals/70/a5/52/70a552e8e955049c8587b2d7606cd6a6.gif?width=424&height=318")
+    .setColor(config.colorEmbeds)
+    .setTimestamp()
+    .setFooter(config.textoFooter)
+    let canalLogs = client.channels.cache.get(config.idCanalLogs);
+    canalLogs.send(embedread);
+
   setInterval(() => {
       let MSGS = [
           `${config.prefijo}ayuda`,
-          `eAE`
+          `Ea`
       ];
       
       client.user.setActivity(MSGS[~~(Math.random() * MSGS.length)], {
