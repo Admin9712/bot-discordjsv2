a95c2827b245929cf5588fe46fe03033b803a641
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-27 03:08:46 +0100

    Ready.js Actualizado

--- eventos/ready.js
+++ eventos/ready.js
@@ -5,7 +5,7 @@ exports.run = async(client) => {
  let today = new Date();
  let data = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
  let tiempo = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  console.log(`[${data}][${tiempo}]Loggeado en ${client.user.tag}`)
+  console.log(`[${data}][${tiempo}]Loggeado en ${client.user.tag} 🤪`)
  let embedread = new Discord.MessageEmbed() 
     .setTitle(`**[BOT INICIADO]**`)
     .setThumbnail("https://images-ext-1.discordapp.net/external/UEu3Cx_CnK2HB4MXx6l1hypTiwG1YWAVO1dIDyDe4h8/https/i.pinimg.com/originals/70/a5/52/70a552e8e955049c8587b2d7606cd6a6.gif?width=424&height=318")
