51216059edbf78a653715c8544c66075ff84718b
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-27 02:21:58 +0100

    EmojiCreate.js Creado

---
+++ eventos/emojiCreate.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const config = require('../config.json');
+
+exports.run = async(client, emoji) => {
+if(!emoji.guild.member(client.user).hasPermission('VIEW_AUDIT_LOG')) return;
+ let tEmoji = emoji.animated ? 'Animado' : 'No Animado';
+ emoji.guild.fetchAuditLogs().then(logs => { 
+  let userID = logs.entries.first().executor.id;
+       let embedemcr = new Discord.MessageEmbed() 
+       .setTitle('**[EMOJI CREADO]**')
+       .setColor(config.colorEmbeds)
+       .setThumbnail(emoji.url)
+       .addField("Nombre de el Emoji:", emoji.name, true)
+       .addField("ID De el Emoji:", `**\`${emoji.id}\`**`, true)
+       .addField("Tipo de Emoji:", `\`${tEmoji}\``, false)
+       .addField("Emoji Creado por:", `**__<@!${userID}>__**`, true)
+       .addField("ID Creador de el Emoji", `**\`${userID}\`**`, true)
+       .setTimestamp()
+       .setFooter(config.textoFooter)
+       let canalLogs = client.channels.cache.get(config.idCanalLogs);
+       canalLogs.send(embedemcr);
+ })
+}
