003936ec934d4f034be0e021c1abb8f2bcbca40e
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-27 02:39:29 +0100

    guildBanRemove.js Creado

---
+++ eventos/guildBanRemove.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const config = require('../config.json');
+
+exports.run = async(client, guild, user) => {
+if(!guild.member(client.user).hasPermission('VIEW_AUDIT_LOG')) return;
+ guild.fetchAuditLogs().then(logs => { 
+  let userID = logs.entries.first().executor.id;
+  let userAvatar = logs.entries.first().executor.avatarURL();
+ let embeusds = new Discord.MessageEmbed() 
+    .setTitle('**[USUARIO DESBANEADO]**')
+    .setColor(config.colorEmbeds)
+    .addField("Usuario Desbaneado:", `<@${user.id}>`, true)
+    .addField("ID Usuario Desbaneado:", `**\`${user.id}\`**`, true)
+    .addField("Staff:", `<@${userID}>`, false)
+    .addField("ID Staff:", `**\`${userID}\`**`, true)
+    .setTimestamp()
+    .setFooter(config.textoFooter)
+       let canalLogs = client.channels.cache.get(config.idCanalLogs);
+       canalLogs.send(embeusds);
+ })
+}
